b773436b7e15b2e647e31de0cb51c7d0e84ca83c
miketeow <27071053+miketeow@users.noreply.github.com>
2024-07-31 22:22:20 +0800

    crawl page recursive working

--- crawl.ts
+++ crawl.ts
@@ -1,28 +1,51 @@
 import { JSDOM } from "jsdom";
-async function crawlPage(currentURL: string) {
+
+async function crawlPage(baseURL: string, currentURL: string, pages: any) {
+  const baseURLObject = new URL(baseURL);
+  const currentURLObject = new URL(currentURL);
+  if (baseURLObject.hostname !== currentURLObject.hostname) {
+    return pages;
+  }
+
+  const normalizeCurrentURL = normalizeURL(currentURL);
+  if (pages[normalizeCurrentURL] > 0) {
+    pages[normalizeCurrentURL]++;
+    return pages;
+  }
+
+  pages[normalizeCurrentURL] = 1;
   console.log(`actively crawling ${currentURL}`);
+
   try {
     const response = await fetch(currentURL);
     if (response.status > 399) {
       console.log(
         `Error in fetch: status code ${response.status}, on page${currentURL}`
       );
-      return;
+      return pages;
     }
     const contentType = response.headers.get("content-type");
     if (!contentType?.includes("text/html")) {
       console.log(
         `Error in fetch: content type ${contentType}, on page${currentURL}`
       );
-      return;
+      return pages;
     }
-    console.log(await response.text());
+    const htmlBody = await response.text();
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
   } catch (error) {
     const errorMessage =
       error instanceof Error ? error.message : "Unknown error";
     console.log(`Error in fetch: ${errorMessage}, on page${currentURL}`);
   }
+  return pages;
 }
+
 function getURLsFromHTML(htmlBody: string, baseURL: string) {
   const urls: string[] = [];
   const dom = new JSDOM(htmlBody);
--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
 import { crawlPage } from "./crawl";
 
-function main() {
+async function main() {
   if (process.argv.length < 3) {
     console.log("no website provided");
     process.exit(1);
   }
   const baseUrl = process.argv[2];
   console.log(`start crawling ${baseUrl}`);
-  crawlPage(baseUrl);
+  const pages = await crawlPage(baseUrl, baseUrl, {});
+
+  for (const page of Object.entries(pages)) {
+    console.log(page);
+  }
 }
 
 main();
