4ed12b8be87e6aa0f302f71cb18d6055454bbd4e
miketeow <27071053+miketeow@users.noreply.github.com>
2024-07-31 19:25:59 +0800

    getURLsFromHTML and tests

--- crawl.test.ts
+++ crawl.test.ts
@@ -1,4 +1,4 @@
-import { normalizeURL } from "./crawl";
+import { getURLsFromHTML, normalizeURL } from "./crawl";
 import { expect, test } from "vitest";
 
 test("normalizeURL strip the protocol", () => {
@@ -27,4 +27,70 @@ test("normalizeURL capitals", () => {
   const actual = normalizeURL(input);
   const expected = "blog.boot.dev/path";
   expect(actual).toBe(expected);
+});
+
+test("getURLsFromHTML absolute url", () => {
+  const input = `
+  <html>
+    <body>
+        <a href="https://blog.boot.dev/path">
+            Boot.dev Blog
+        </a>
+    </body>
+  </html>`;
+  const inputBaseUrl = "https://blog.boot.dev/path";
+  const actual = getURLsFromHTML(input, inputBaseUrl);
+  const expected = ["https://blog.boot.dev/path"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative url", () => {
+  const input = `
+    <html>
+      <body>
+          <a href="/path/">
+              Boot.dev Blog
+          </a>
+      </body>
+    </html>`;
+  const inputBaseUrl = "https://blog.boot.dev";
+  const actual = getURLsFromHTML(input, inputBaseUrl);
+  const expected = ["https://blog.boot.dev/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative both", () => {
+  const input = `
+      <html>
+        <body>
+            <a href="https://blog.boot.dev/path1">
+                Boot.dev Blog Path One
+            </a>
+            <a href="/path2/">
+                Boot.dev Blog Path Two
+            </a>
+        </body>
+      </html>`;
+  const inputBaseUrl = "https://blog.boot.dev";
+  const actual = getURLsFromHTML(input, inputBaseUrl);
+  const expected = [
+    "https://blog.boot.dev/path1",
+    "https://blog.boot.dev/path2/",
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML invalid", () => {
+  const input = `
+      <html>
+        <body>
+            <a href="invalid">
+                Boot.dev Blog
+            </a>
+        </body>
+      </html>`;
+  const inputBaseUrl = "https://blog.boot.dev";
+  const actual = getURLsFromHTML(input, inputBaseUrl);
+  const expected: string[] = [];
+  expect(actual).toEqual(expected);
 });
--- crawl.ts
+++ crawl.ts
@@ -1,3 +1,29 @@
+import { JSDOM } from "jsdom";
+function getURLsFromHTML(htmlBody: string, baseURL: string) {
+  const urls: string[] = [];
+  const dom = new JSDOM(htmlBody);
+  const document = dom.window.document;
+  const anchors = document.querySelectorAll("a");
+  for (const anchor of anchors) {
+    const href = anchor.getAttribute("href");
+    if (href?.slice(0, 1) === "/") {
+      urls.push(`${baseURL}${href}`);
+    } else {
+      try {
+        const urlObject = new URL(href!);
+        urls.push(href!);
+      } catch (error) {
+        console.log(`Invalid URL: ${href}`);
+      }
+    }
+  }
+  //   const regex = /<a\s+href="([^"]+)"/g;
+  //   let match;
+  //   while ((match = regex.exec(htmlBody)) !== null) {
+  //     urls.push(normalizeURL(match[1]));
+  //   }
+  return urls;
+}
 function normalizeURL(urlString: string) {
   const urlObject = new URL(urlString);
 
@@ -8,4 +34,4 @@ function normalizeURL(urlString: string) {
   return hostPath;
 }
 
-export { normalizeURL };
+export { normalizeURL, getURLsFromHTML };
